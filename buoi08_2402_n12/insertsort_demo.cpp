55b9d9cb6c9a03932d1c67a15e1e4d9eaf428ae1
Tai Do <donhutai@gmail.com>
2025-02-24 09:05:05 +0000

    buoi08/bai1

--- buoi08_2402_n12/insertsort_demo.cpp
+++ buoi08_2402_n12/insertsort_demo.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <ctime> // time(NULL)
+#include <cstdlib> // srand, rand
 using namespace std;
 
 void insertion_sort(int a[], int n)
@@ -53,6 +55,27 @@ void xuat_mang(int a[], int n)
         printf("%d ", a[i]);
 }
 
+void sinh_mang(int a[], int n, int vfrom, int vto)
+{
+    for(int i=0;i<n;i++)// a->b: rand()%(b-a+1) [0,b-a]+a -> [a, b]
+        a[i]= rand()%(vto-vfrom+1) + vfrom;
+}
+
+void test3_cnt(int n, int t, long long *cnt_ss, long long *cnt_dc) {
+    int a[100001] = {};
+    *cnt_ss = 0;
+    *cnt_dc = 0;
+    for(int i=0; i<t; i++){
+        int ss, dc;
+        sinh_mang(a, n, 0, 100000);
+        insertion_sort_cnt(a, n, &ss, &dc);
+        (*cnt_ss) += ss;
+        (*cnt_dc) += dc;
+    }
+    *cnt_ss = (*cnt_ss) / t;
+    *cnt_dc = *cnt_dc / t;
+}
+
 void test1()
 {
     int a[] = {6, 8, 1, 5, 6, 2, 7, 4}, n = 8;
@@ -76,10 +99,26 @@ void test2()
     printf("\n");
 }
 
+void test3()
+{
+    printf("----- TEST 3 -----\n");
+    int n = 4000, t = 10;
+    long long n_ss, n_dc;
+    
+    printf("So lan thuc nghiem: %d\n", t);
+    printf("Kich thuoc mang: %d\n", n);
+    test3_cnt(n, t, &n_ss, &n_dc);
+    printf("So lan so sanh trung binh: %lld\n", n_ss);
+    printf("So lan dich chuyen trung binh: %lld\n", n_dc);
+}
+
 int main()
 {
+    srand(time(NULL));
+
     printf("--- INSERT SORT --- \n");
     // test1();
-    test2();
+    // test2();
+    test3();
     return 0;
 }
